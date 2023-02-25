0fd34f3a29b51ca1f0e305627b3644d2d905f1df
陳建佑 <yuyuwebtest@gmail.com>
2023-02-25 10:26:49 +0800

    路由修改為hash

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div>
-    <h1>app</h1>
+    <RouterView />
   </div>
 
-  <div class="container py-4 px-3 mx-auto">
-      <h1 class="py-5">Hello, Bootstrap and Vite!</h1>
-      <button class="btn btn-primary">Primary button</button>
-      <button class="btn btn-danger">Primary button</button>
-    </div>
 </template>
 
 <style scoped>
